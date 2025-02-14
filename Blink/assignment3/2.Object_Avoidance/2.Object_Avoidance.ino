944669f19f9917b984d31d0adcfee352e412fbac
amirranjbarmaki <amir.ranjbar.maki@student.nhlstenden.com>
2025-02-14 10:28:04 +0100

    change the hardcode

--- Blink/assignment3/2.Object_Avoidance/2.Object_Avoidance.ino
+++ Blink/assignment3/2.Object_Avoidance/2.Object_Avoidance.ino
@@ -4,6 +4,9 @@ const int MOTOR_B1 = 5;
 const int MOTOR_B2 = 6;
 const int TRIG = 7;
 const int ECHO = 9;
+const int MOTOR_A_SPEED = 250;
+const int MOTOR_B_SPEED = 237;
+const int ROTATION_SPEED = 200;
 
 void setup() {
     pinMode(TRIG, OUTPUT);
@@ -58,16 +61,16 @@ long getDistance() {
 // Motor control functions
 void moveForward() {
     analogWrite(MOTOR_A1, 0);  // Left motor forward (A2 ON, A1 OFF)
-    analogWrite(MOTOR_A2, 250);
-    analogWrite(MOTOR_B1, 237); // Right motor forward (B1 ON, B2 OFF)
+    analogWrite(MOTOR_A2, MOTOR_A_SPEED);
+    analogWrite(MOTOR_B1, MOTOR_B_SPEED); // Right motor forward (B1 ON, B2 OFF)
     analogWrite(MOTOR_B2, 0);
 }
 
 void moveBackward() {
-    analogWrite(MOTOR_A1, 250); // Left motor backward (A1 ON, A2 OFF)
+    analogWrite(MOTOR_A1, MOTOR_A_SPEED); // Left motor backward (A1 ON, A2 OFF)
     analogWrite(MOTOR_A2, 0);
     analogWrite(MOTOR_B1, 0);   // Right motor backward (B2 ON, B1 OFF)
-    analogWrite(MOTOR_B2, 250);
+    analogWrite(MOTOR_B2, MOTOR_B_SPEED);
 }
 
 void stopMotors() {
@@ -79,17 +82,17 @@ void stopMotors() {
 
 
 void turnLeft() {
-    analogWrite(MOTOR_A1, 200);
+    analogWrite(MOTOR_A1, ROTATION_SPEED);
     analogWrite(MOTOR_A2, 0);  // Left motor backward
-    analogWrite(MOTOR_B1, 200);
+    analogWrite(MOTOR_B1, ROTATION_SPEED);
     analogWrite(MOTOR_B2, 0);    // Right motor forward
 }
 
 void turnRight() {
     analogWrite(MOTOR_A1, 0);
-    analogWrite(MOTOR_A2, 200);    // Left motor forward
+    analogWrite(MOTOR_A2, ROTATION_SPEED);    // Left motor forward
     analogWrite(MOTOR_B1, 0);
-    analogWrite(MOTOR_B2, 200);  // Right motor backward
+    analogWrite(MOTOR_B2, ROTATION_SPEED);  // Right motor backward
 }
 
 
